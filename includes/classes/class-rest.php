356d6fdd0c774fcd7572a19f553ca2a0b493c4db
Connor Meehan <cmeehan@elon.edu>
2022-03-27 15:55:29 +0000

    Adding member directory profile to rest

--- includes/classes/class-rest.php
+++ includes/classes/class-rest.php
@@ -14,6 +14,129 @@ class OFRC_REST{
 			'callback'	=> array($this, 'get_archives'),
 			'permission_callback'	=> '__return_true'
 		));	
+		
+		register_rest_route('ofrc/v1', '/mobile-sign-in', array(
+			'methods'	=> array('GET', 'POST'),
+			'callback'	=> array($this, 'mobile_sign_in'),
+			'permission_callback'	=> '__return_true'
+		));	
+		
+		register_rest_route('ofrc/v1', 'directory-feed', array(
+			'methods'	=> array('GET', 'POST'), 
+			'callback'	=> array($this, 'get_directory_feed'), 
+			'permission_callback'	=> function(WP_REST_Request $request){				
+				return user_can($request->get_param('user_id'), 'level_0');
+			}
+		) );
+		
+		register_rest_route('ofrc/v1', 'directory-feed/get-member', array(
+			'methods'	=> array('GET', 'POST'), 
+			'callback'	=> array($this, 'get_member_from_directory'), 
+			'permission_callback'	=> function(WP_REST_Request $request){				
+				return user_can($request->get_param('user_id'), 'level_0');
+			}
+		) );
+	}
+	
+	/**
+	 * Get an individual member from the directory 
+	 * 
+	 * @params WP_REST_Request 
+	 * @returns json - member profile
+	 */
+	public function get_member_from_directory(WP_REST_Request $request){
+		
+		if(!$request->get_params() || !$request->get_param('member_id')){
+			return wp_send_json_error( array('response' => 'invalid_member_id') );
+		}
+		
+		$member_id = $request->get_param('member_id');
+		
+		
+		$name = '';
+		
+		if(get_field('prefix', 'user_' . $member_id)){
+			$name .= get_field('prefix', 'user_' . $member_id) . ' ';
+		}
+		
+		if(get_field('first_name', 'user_' . $member_id)){
+			$name .= get_field('first_name', 'user_' . $member_id);
+		}
+		
+		if(get_field('last_name', 'user_' . $member_id)){
+			$name .= ' ' . get_field('last_name', 'user_' . $member_id);
+		}
+		
+		if(get_field('suffix', 'user_' . $member_id)){
+			$name .= ' ' . get_field('suffix', 'user_' . $member_id);
+		}
+		$member_profile = array(
+			'member_name'	=> $name,
+			'mobile_phone'	=> get_field('mobile_phone', 'user_' . $member_id),
+			'home_phone'	=> get_field('home_phone', 'user_' . $member_id),
+			'work_phone'	=> get_field('work_phone', 'user_' . $member_id),
+			'birthday'		=> date_format(date_create(get_field('birthday', 'user_' . $member_id)), 'F d, Y'),
+			'is_birthday'	=> date('Y-m-d') == get_field('birthday', 'user_' . $member_id),
+			'gender'		=> get_field('gender', 'user_' . $member_id), 
+			'email_address'	=> get_field('email_address', 'user_' . $member_id), 
+			'profile_picture'	=> get_field('profile_picture', 'user_' . $member_id) ?  get_field('profile_picture', 'user_' . $member_id)['url'] : false,
+			'biography'		=> get_field('biography', 'user_' . $member_id)
+		);
+		
+		return wp_send_json_success($member_profile);
+	}
+	
+	public function mobile_sign_in(WP_REST_Request $request){
+		$username = $request->get_param('username');
+		$password = $request->get_param('password');
+		
+		$user = wp_authenticate( $username, $password );
+
+		return !is_wp_error($user) ? wp_send_json_success( $user->ID ) : wp_send_json_error( false );
+	}
+	
+	/**
+	 * Get the directory feed 
+	 * 
+	 * @params WP_REST_Request 
+	 * 
+	 * @returns json 
+	 */
+	public function get_directory_feed(WP_REST_Request $request){
+		
+		$user_query_args = array(
+			'role' => 'member',
+			'orderby'	=> 'meta_value', 
+			'meta_key'	=> 'last_name',
+			'order'	=> 'ASC'
+		);
+		
+		// Get any search parameters 
+		if($request->get_param('search_terms')){
+			
+		}
+		
+		$user_query = new WP_User_Query($user_query_args);
+		
+		$users = array();
+		
+		if($user_query->get_results()){
+			foreach($user_query->get_results() as $user){
+				$user_id = $user->ID;
+				
+				
+				$users[] = $u = array(
+					'user_id'	=> $user_id,
+					'first_name' => get_field('first_name', 'user_' . $user_id),
+					'last_name' => get_field('last_name', 'user_' . $user_id),
+					'profile_picture' => get_field('profile_picture', 'user_' . $user_id)['url'],
+				);
+			}
+		}
+		
+		// Get the directory 
+		
+		return wp_send_json_success($users);
 	}
 	
 	public function get_archives(){
