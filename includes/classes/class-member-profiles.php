e4414bb6afa15b02e2b8670c9d53e8b8e396f360
Connor Meehan <cmeehan@elon.edu>
2022-05-09 14:48:51 +0000

    Updating assets and adding member cpt

--- includes/classes/class-member-profiles.php
+++ includes/classes/class-member-profiles.php
@@ -10,6 +10,73 @@ class OFRC_Member_Profiles{
 		add_action('wp_ajax_nopriv_member_login', array($this, 'member_login'));
 		add_action('wp_ajax_signout', array($this, 'member_signout'));
 		add_action('wp_ajax_nopriv_signout', array($this, 'member_signout'));
+		add_action('init', array($this, 'register_member_profile_cpt'));
+		
+	}
+	
+	
+	
+	// Register Custom Post Type
+	function register_member_profile_cpt() {
+	
+		$labels = array(
+			'name'                  => _x( 'Members', 'Post Type General Name', OFRC_TEXTDOMAIN ),
+			'singular_name'         => _x( 'Member', 'Post Type Singular Name', OFRC_TEXTDOMAIN ),
+			'menu_name'             => __( 'Members', OFRC_TEXTDOMAIN ),
+			'name_admin_bar'        => __( 'Member', OFRC_TEXTDOMAIN ),
+			'archives'              => __( 'Member Archives', OFRC_TEXTDOMAIN ),
+			'attributes'            => __( 'Member Attributes', OFRC_TEXTDOMAIN ),
+			'parent_item_colon'     => __( 'Parent Profile:', OFRC_TEXTDOMAIN ),
+			'all_items'             => __( 'All Profiles', OFRC_TEXTDOMAIN ),
+			'add_new_item'          => __( 'Add New Profile', OFRC_TEXTDOMAIN ),
+			'add_new'               => __( 'Add New', OFRC_TEXTDOMAIN ),
+			'new_item'              => __( 'New Profile', OFRC_TEXTDOMAIN ),
+			'edit_item'             => __( 'Edit Profile', OFRC_TEXTDOMAIN ),
+			'update_item'           => __( 'Update Profile', OFRC_TEXTDOMAIN ),
+			'view_item'             => __( 'View Profile', OFRC_TEXTDOMAIN ),
+			'view_items'            => __( 'View Profiles', OFRC_TEXTDOMAIN ),
+			'search_items'          => __( 'Search Profile', OFRC_TEXTDOMAIN ),
+			'not_found'             => __( 'Not found', OFRC_TEXTDOMAIN ),
+			'not_found_in_trash'    => __( 'Not found in Trash', OFRC_TEXTDOMAIN ),
+			'featured_image'        => __( 'Featured Image', OFRC_TEXTDOMAIN ),
+			'set_featured_image'    => __( 'Set featured image', OFRC_TEXTDOMAIN ),
+			'remove_featured_image' => __( 'Remove featured image', OFRC_TEXTDOMAIN ),
+			'use_featured_image'    => __( 'Use as featured image', OFRC_TEXTDOMAIN ),
+			'insert_into_item'      => __( 'Insert into profile', OFRC_TEXTDOMAIN ),
+			'uploaded_to_this_item' => __( 'Uploaded to this profile', OFRC_TEXTDOMAIN ),
+			'items_list'            => __( 'Profiles list', OFRC_TEXTDOMAIN ),
+			'items_list_navigation' => __( 'Profiles list navigation', OFRC_TEXTDOMAIN ),
+			'filter_items_list'     => __( 'Filter profiles list', OFRC_TEXTDOMAIN ),
+		);
+		$rewrite = array(
+			'slug'                  => 'member',
+			'with_front'            => true,
+			'pages'                 => true,
+			'feeds'                 => true,
+		);
+		$args = array(
+			'label'                 => __( 'Member', OFRC_TEXTDOMAIN ),
+			'description'           => __( 'Post Type Description', OFRC_TEXTDOMAIN ),
+			'labels'                => $labels,
+			'supports'              => array( 'title', 'editor', 'revisions', 'custom-fields', 'page-attributes' ),
+			'hierarchical'          => false,
+			'public'                => true,
+			'show_ui'               => true,
+			'show_in_menu'          => true,
+			'menu_position'         => 5,
+			'menu_icon'             => 'dashicons-groups',
+			'show_in_admin_bar'     => true,
+			'show_in_nav_menus'     => true,
+			'can_export'            => true,
+			'has_archive'           => true,
+			'exclude_from_search'   => false,
+			'publicly_queryable'    => true,
+			'rewrite'               => $rewrite,
+			'capability_type'       => 'page',
+			'show_in_rest'          => false,
+		);
+		register_post_type( 'member', $args );
+	
 	}
 	
 		
